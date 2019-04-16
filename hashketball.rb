7bde37472bf32c24563432a214481a11ff4e4fcb
Kane Koo <kanekoo@gmail.com>
2019-04-16 03:06:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,7 @@ def player_numbers (team)
    game_hash.each do |location, team_data|
       if team_data[:team_name] == team
           team_data[:players].each do |pname, stats|
-            binding.pry
+   #         binding.pry
             jers_numb_arr << stats[:number]
            end
           end
