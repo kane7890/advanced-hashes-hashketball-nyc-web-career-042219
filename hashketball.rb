da89327974ea9309608382aa6770000417d666f6
Kane Koo <kanekoo@gmail.com>
2019-04-16 03:37:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -216,13 +216,14 @@ def player_numbers (team)
 def player_stats (name)
   statbox={}
    game_hash.each do |location, team_data|
-     binding.pry
-#    team_data[:players].each do |pname, stats|
+    # phas = team_data[:players]
+    
+   team_data[:players].each do |pname, stats|
        if pname == name 
   #       binding.pry
           statbox = stats
-        end
-      end
+``    end
+     end
    # binding.pry
    # statbox = team_data[:players][:name]
     end
